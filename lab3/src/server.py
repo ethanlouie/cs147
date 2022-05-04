aba50e630d6aa2a0897e8d74342bee3bf5d1a945
Ubuntu <ubuntu@ip-172-31-6-4.us-west-1.compute.internal>
2022-05-04 23:03:03 +0000

    updated .gitignore

--- lab3/src/server.py
+++ lab3/src/server.py
@@ -1,9 +1,12 @@
-
+from requests import get
 from flask import Flask
 from flask import request
 
-app = Flask(__name__)
+# print public IP address
+print(f'Public IP: {get('https://api.ipify.org').text}')
 
+# Flask Server
+app = Flask(__name__)
 @app.route("/")
 def hello():
 	print(request.args.get("temp"))
