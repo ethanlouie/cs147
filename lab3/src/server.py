d513335213cb487a2c59d17be582c9f6ac83ef16
Ubuntu <ubuntu@ip-172-31-6-4.us-west-1.compute.internal>
2022-05-04 22:49:13 +0000

    added python flask server code

--- lab3/src/server.py
+++ lab3/src/server.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	print(request.args.get("var"))
-	return "We received value: "+str(request.args.get("var"))
+	print(request.args.get("temp"))
+	print(request.args.get("humidity"))
+	return "We received temp: "+str(request.args.get("temp")) + ', humidity: ' +str(request.args.get("humidity"))
 
